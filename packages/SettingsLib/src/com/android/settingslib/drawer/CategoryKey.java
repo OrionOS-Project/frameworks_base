a3b16c575b0482c08536eb956449823350282449
XlxFoXxlX <joshfox87@gmail.com>
2017-09-10 09:43:06 -0400

    base: Add Orion Molecular CategoryKey
    -needed to use our own Key for Orion Molecular dashboard entry
    
    Change-Id: I262f65d75b46f3f8958fb929b335ae20e4d378ab
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -74,6 +74,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.more_security_privacy_settings";
     public static final String CATEGORY_SUPERVISION =
             "com.android.settings.category.ia.supervision";
+    public static final String CATEGORY_ORION =
+            "com.android.settings.category.ia.orion";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
