989f38a7bf8b6c190be320299f3e8c2fd4f628b7
Chirayu Desai <chirayudesai1@gmail.com>
2020-02-20 05:49:26 +0530

    Add a panic trigger to Global actions
    * Toggled from Settings -> Security
    * Launches Ripple
    
    Also includes:
    commit 819535d1701a8dda98fd6d296a634f1f407577ee
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Tue Jan 5 19:55:01 2021 +0530
    
        GlobalActions: Use addIfShouldShowAction for Panic trigger
    
        Change-Id: I7c66428024ea181e753bd23b44c99f3bc6f8a6dc
        (cherry picked from commit 4dcadf7e6f0cac889c1f1108cae205311f5054f9)
    
    commit c0992a6fdfca5706cfacde0e35a62a5b20a4cfb2
    Author: Oliver Scott <olivercscott@gmail.com>
    Date:   Fri Jan 15 14:28:34 2021 -0500
    
        GlobalActions: Add forked panic trigger package name
    
        Issue: calyxos#275
        Change-Id: I26abffff937ee08389d3f549c5eda530ec7304a5
        (cherry picked from commit 616cc12253eb8ca01c16b9cdc5ad7eff350bc722)
    
    commit a43fdfa6bfd480e0517c5ae7b813dc86a5dd2501
    Author: Chirayu Desai <chirayudesai1@gmail.com>
    Date:   Mon May 10 23:01:32 2021 +0530
    
        GlobalActions: Only match system apps for panic trigger
    
        Change-Id: I8de9ce50151eaaba4e74b18f4ba6db13f00683d6
        (cherry picked from commit 4107dab95d16aab6887c71c8fb7c1879facd3baf)
    
    Change-Id: Ib919f87573ce92511bd75bd5df8177b66cc156ec

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -46,6 +46,7 @@ import android.app.UiModeManager;
 import android.app.WallpaperManager;
 import android.app.trust.TrustManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -181,6 +182,8 @@ import java.util.concurrent.Executor;
 
 import javax.inject.Inject;
 
+import lineageos.providers.LineageSettings;
+
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that may show depending
  * on whether the keyguard is showing, and whether the device is provisioned.
@@ -211,6 +214,13 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
     private static final String RESTART_ACTION_KEY_RESTART_DOWNLOAD = "restart_download";
     private static final String RESTART_ACTION_KEY_RESTART_FASTBOOT = "restart_fastboot";
     private static final String RESTART_ACTION_KEY_RESTART_SYSTEMUI = "restart_systemui";
+    private static final String GLOBAL_ACTION_KEY_PANIC = "panic";
+
+    private static final String[] PANIC_PACKAGES =
+            new String[]{"info.guardianproject.ripple", "org.calyxos.ripple"};
+    private static String PANIC_PACKAGE;
+    private static final String PANIC_ACTIVITY = "org.calyxos.ripple.CountDownActivity";
+    private static final String PANIC_SETTINGS = "org.calyxos.ripple.SettingsActivityLink";
 
     // See NotificationManagerService#scheduleDurationReachedLocked
     private static final long TOAST_FADE_TIME = 333;
@@ -779,6 +789,12 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 addIfShouldShowAction(tempActions, new StandbyAction());
             } else if (GLOBAL_ACTION_KEY_DEVICECONTROLS.equals(actionKey)) {
                 addIfShouldShowAction(tempActions, new DeviceControlsAction());
+            } else if (GLOBAL_ACTION_KEY_PANIC.equals(actionKey)) {
+                if (LineageSettings.Secure.getIntForUser(mContext.getContentResolver(),
+                        LineageSettings.Secure.PANIC_IN_POWER_MENU, 0, getCurrentUser().id) != 0
+                        && isPanicAvailable()) {
+                    addIfShouldShowAction(tempActions, new PanicAction());
+                }
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -936,6 +952,20 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         return user != null && user.isAdmin()
                 && mSecureSettings.getIntForUser(Settings.Secure.BUGREPORT_IN_POWER_MENU, 0,
                 user.id) != 0;
+    }
+
+    private boolean isPanicAvailable() {
+        for (String panicPackage : PANIC_PACKAGES) {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(panicPackage, PANIC_ACTIVITY));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            if (mContext.getPackageManager().resolveActivity(intent,
+                    PackageManager.MATCH_SYSTEM_ONLY) != null) {
+                PANIC_PACKAGE = panicPackage;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -1563,6 +1593,48 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         @Override
         public boolean showBeforeProvisioning() {
             return true;
+        }
+    }
+
+    private final class PanicAction extends SinglePressAction implements LongPressAction  {
+        private PanicAction() {
+            super(com.android.systemui.res.R.drawable.ic_panic,
+                    com.android.systemui.res.R.string.global_action_panic);
+        }
+
+        @Override
+        public boolean showDuringKeyguard() {
+            return false;
+        }
+
+        @Override
+        public boolean showBeforeProvisioning() {
+            return false;
+        }
+
+        @Override
+        public void onPress() {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(PANIC_PACKAGE, PANIC_ACTIVITY));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            if (mContext.getPackageManager().resolveActivity(intent,
+                    PackageManager.MATCH_SYSTEM_ONLY) != null) {
+                mContext.startActivity(intent);
+            }
+        }
+
+        @Override
+        public boolean onLongPress() {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(PANIC_PACKAGE, PANIC_SETTINGS));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            if (mContext.getPackageManager().resolveActivity(intent,
+                    PackageManager.MATCH_SYSTEM_ONLY) != null) {
+                mContext.startActivity(intent);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
