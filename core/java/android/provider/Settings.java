0f2025fc779d63c851dca11483d349aa1b608a44
jhenrique09 <jhenrique09@users.noreply.github.com>
2022-01-18 20:46:03 +0800

    base: Implement cutout force full screen [1/2]
    Inspired by MIUI and Essential
    
    CutoutFullscreenController: Adapted from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    
    [cjh1249131356]: Adapt to A12
    
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Sat, 21 Apr 2018 23:45:57 -0700
    
        Add an option to force pre-O apps to use full screen aspect ratio
    
        When an app target pre-O releases, the default max aspect ratio
        is 1.86:1 which leads to ugly black areas on devices that have
        screens with higher aspect ratio (for example Galaxy S8/S9).
    
        This change adds an option to allow users to change aspect ratio
        for pre-O apps to full screen aspect ratio.
    
        Change-Id: I41d5408841593a12443be885e11959bffaebb67b
    
    [ghotsrider-reborn]
    - Use LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS instead of SHORT_EDGES
    - Misc code cleanup
    
    Change-Id: Ia93a550cc952962de4fa270aae37a7b8c9ba45ac
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6791,6 +6791,12 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * Force full screen for devices with cutout
+         * @hide
+         */
+        public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6944,6 +6950,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(PREFERRED_REGION);
             PRIVATE_SETTINGS.add(MOUSE_SCROLLING_ACCELERATION);
             PRIVATE_SETTINGS.add(MOUSE_SCROLLING_SPEED);
+            PRIVATE_SETTINGS.add(FORCE_FULLSCREEN_CUTOUT_APPS);
         }
 
         /**
--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19962,4 +19962,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
         r.getWindowProcessController().setOptimizationInfo(compilerFilter, compilationReason);
     }
+
+    public boolean shouldForceCutoutFullscreen(String packageName) {
+        return mActivityTaskManager.shouldForceCutoutFullscreen(packageName);
+    }
 }
