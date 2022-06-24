e99b9e29ab8936143bded5d065f72d19eb025d3b
Anushek Prasal <anushekprasal@gmail.com>
2022-06-24 18:25:26 +0530

    base: Vibrate for Torch gesture
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7394,6 +7394,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         try {
             final String rearFlashCameraId = getRearFlashCameraId();
             if (rearFlashCameraId != null) {
+                performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, "Flashlight toggle");
                 mCameraManager.setTorchMode(rearFlashCameraId, !mTorchEnabled);
                 mTorchEnabled = !mTorchEnabled;
             }
