6eb55279c688c3fdbf9f6a249a2b3944c4d80597
t-m-w <tmw-likes-calyx@pm.me>
2022-08-26 15:45:17 +0000

    base: Bluetooth timeout feature
    Add Bluetooth timeout setting so it can be accessed by the Bluetooth
    APEX module.
    
    Change-Id: I81941dc4e390b17e4ec8f57bf45327f3b8b525db

--- packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/GlobalSettings.java
@@ -134,5 +134,6 @@ public class GlobalSettings {
         Settings.Global.MINMODE_ACTIVE,
         Settings.Global.WATCH_RANGING_AVAILABLE,
         Settings.Global.WIFI_OFF_TIMEOUT,
+        Settings.Global.BLUETOOTH_OFF_TIMEOUT,
     };
 }
