2e7b66efd1acf8575921f5736c81b691ef999bcb
Lars Greiss <kufikugel@googlemail.com>
2022-11-03 15:42:03 +0800

    SystemUI: HeadsUp timeout option [1/2]
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7227,6 +7227,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_CHIP = "statusbar_clock_chip";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
