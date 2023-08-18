28350d7400ae1832287e593fe3a92889f9dc25e8
DarkJoker360 <simoespo159@gmail.com>
2023-08-18 09:30:26 +0000

    SystemUI: Launch power menu from QS only if enabled
    * When power menu is hidden on secure lockscreen,
      it was still possible to launch it from QS.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -17,8 +17,10 @@
 package com.android.systemui.qs.footer.ui.viewmodel
 
 import android.content.Context
+import android.os.UserHandle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.provider.Settings
 import android.util.Log
 import android.view.ContextThemeWrapper
 import androidx.lifecycle.DefaultLifecycleObserver
@@ -48,6 +50,7 @@ import com.android.systemui.qs.panels.ui.viewmodel.TextFeedbackContentViewModel.
 import com.android.systemui.qs.panels.ui.viewmodel.TextFeedbackViewModel
 import com.android.systemui.res.R
 import com.android.systemui.shade.ShadeDisplayAware
+import com.android.systemui.statusbar.policy.KeyguardStateController
 import com.android.systemui.user.domain.interactor.HeadlessSystemUserMode
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor
 import com.android.systemui.util.icuMessageFormat
@@ -135,6 +138,7 @@ class FooterActionsViewModel(
         private val selectedUserInteractor: SelectedUserInteractor,
         private val hsum: HeadlessSystemUserMode,
         @Named(PM_LITE_ENABLED) private val showPowerButton: Boolean,
+        private val keyguardStateController: KeyguardStateController
     ) {
         /** Create a [FooterActionsViewModel] bound to the lifecycle of [lifecycleOwner]. */
         fun create(lifecycleOwner: LifecycleOwner): FooterActionsViewModel {
@@ -164,6 +168,7 @@ class FooterActionsViewModel(
                 showPowerButton,
                 selectedUserInteractor,
                 hsum,
+                keyguardStateController,
             )
         }
 
@@ -191,6 +196,7 @@ class FooterActionsViewModel(
                 showPowerButton,
                 selectedUserInteractor,
                 hsum,
+                keyguardStateController,
             )
         }
     }
@@ -206,6 +212,7 @@ fun createFooterActionsViewModel(
     showPowerButton: Boolean,
     selectedUserInteractor: SelectedUserInteractor,
     hsum: HeadlessSystemUserMode,
+    keyguardStateController: KeyguardStateController
 ): FooterActionsViewModel {
 
     val vibrator = appContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
@@ -264,6 +271,11 @@ fun createFooterActionsViewModel(
     }
 
     fun onPowerButtonClicked(expandable: Expandable) {
+        if (keyguardStateController.isShowing() && keyguardStateController.isMethodSecure() 
+                && Settings.System.getIntForUser(appContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT) == 0) {
+            return
+        }
         if (falsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
             return
         }
