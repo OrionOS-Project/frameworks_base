856b1600445120137f5ceb992144822269e2490d
dantmnf <dantmnf2@gmail.com>
2024-01-14 18:58:32 +0800

    PhoneWindowManager: Check camera availability on camera key long press
    And don't launch camera when camera is in use.
    
    Change-Id: I55a96d6c777104155d879251400956713cb119b5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -858,6 +858,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mLongSwipeDown;
     private CameraAvailbilityListener mCameraAvailabilityListener;
 
+    private CameraAvailbilityListener mCameraAvailabilityListener;
+
     private class PolicyHandler extends Handler {
 
         private PolicyHandler(Looper looper) {
