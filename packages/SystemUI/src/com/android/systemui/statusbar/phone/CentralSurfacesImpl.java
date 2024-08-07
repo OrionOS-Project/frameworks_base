ab1e8d136cccaed7e8911729f10f466ec20dbeb5
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2024-08-07 10:18:49 +0530

    SystemUI: Add 7-days Reboot Reminder/Suggestion
    Personal preference :p
    
    TODO:
    - Make it configurable (maybe).
    
    Change-Id: Ia619d851302e7ae57867b0ee0bb4ce40a76edd2f
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -229,6 +229,7 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
+import com.android.systemui.orion.RebootSuggestion;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
@@ -438,6 +439,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final NotificationsController mNotificationsController;
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
+    private final RebootSuggestion mRebootSuggestion;
 
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
@@ -865,6 +867,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
         mWindowManager = windowManager;
         mWindowManagerProvider = windowManagerProvider;
+
+        mRebootSuggestion = new RebootSuggestion(mContext);
     }
 
     private void initBubbles(Bubbles bubbles) {
