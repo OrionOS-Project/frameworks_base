6536924337c97b7980a81ef6ab8092a55fbc999d
gang.huang <gang.huang@unisoc.com>
2024-10-08 12:03:55 +0800

    Fix FD leaks in BootReceiver.
    Bug: 372123174
    Test: Manual.
    Google: 3298172
    Change-Id: I32e8030c025ad2403c961eadacafc97abdf76085
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- services/core/java/com/android/server/BootReceiver.java
+++ services/core/java/com/android/server/BootReceiver.java
@@ -50,6 +50,7 @@ import com.android.modules.utils.TypedXmlPullParser;
 import com.android.modules.utils.TypedXmlSerializer;
 import com.android.server.am.DropboxRateLimiter;
 
+import libcore.io.IoUtils;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -152,6 +153,10 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        if (!Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            return;
+        }
+
         // Log boot events in the background to avoid blocking the main thread with I/O
         new Thread() {
             @Override
@@ -218,6 +223,8 @@ public class BootReceiver extends BroadcastReceiver {
                 } catch (Exception e) {
                     Slog.wtf(TAG, "Error watching for trace events", e);
                     return 0;  // Unregister the handler.
+                } finally {
+                    IoUtils.closeQuietly(fd);
                 }
                 return OnFileDescriptorEventListener.EVENT_INPUT;
             }
