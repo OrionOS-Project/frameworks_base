246db6ed366b5c3354d6b2735e2e9990881e24aa
Praditia Nur <pnplusplus@gmail.com>
2024-11-06 13:34:25 +0100

    core: Import PropImitationHooks [SQUASHED]
    Imported from https://github.com/KusumaOS/android_frameworks_base.
    Also improvement some code to easier for use.
    
    Signed-off-by: Iverz <asterixiverz@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -282,6 +282,7 @@ import com.android.server.uri.UriGrantsManagerService;
 import com.android.server.usage.StorageStatsService;
 import com.android.server.usage.UsageStatsService;
 import com.android.server.usb.UsbService;
+import com.android.server.utils.PropImitationService;
 import com.android.server.utils.TimingsTraceAndSlog;
 import com.android.server.vibrator.VibratorManagerService;
 import com.android.server.voiceinteraction.VoiceInteractionManagerService;
@@ -3401,6 +3402,11 @@ public final class SystemServer implements Dumpable {
         } catch (Throwable e) {
             reportWtf("starting System UI", e);
         }
+        t.traceEnd();
+
+        // PropImitiationService
+        t.traceBegin("PropImitationService");
+        mSystemServiceManager.startService(PropImitationService.class);
         t.traceEnd();
 
         t.traceEnd(); // startOtherServices
