OrionOS-Project/frameworks_base

commit e517138781d57432ef8360268dc91d41b70cb313

Update carrier privilege check in enforceSubscriptionPlanAccess
Remove isEmbedded() check, since this can be called on both PSIM and
ESIM. Replace SM#canManageSubscription with TM#hasCarrierPrivileges,
since it is a superset of the checks done in SM#canManageSubscription
and applies to both PSIMs as well as ESIMS. Updated the documentation
for the two APIs to clarify the differences.

Test: atest CtsTelephonyTestCases (SubscriptionManagerTest)
Fix: 180451446
Change-Id: Icb4e2c619dacaea9e74cedea31865ec1eb689dce

services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -3067,23 +3067,19 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         // Verify they're not lying about package name
         mAppOps.checkPackage(callingUid, callingPackage);
 
-        final SubscriptionManager sm;
-        final SubscriptionInfo si;
         final PersistableBundle config;
+        final TelephonyManager tm;
         final long token = Binder.clearCallingIdentity();
         try {
-            sm = mContext.getSystemService(SubscriptionManager.class);
-            si = sm.getActiveSubscriptionInfo(subId);
             config = mCarrierConfigManager.getConfigForSubId(subId);
+            tm = mContext.getSystemService(TelephonyManager.class);
         } finally {
             Binder.restoreCallingIdentity(token);
         }
 
-        // First check: is caller the CarrierService?
-        if (si != null) {
-            if (si.isEmbedded() && sm.canManageSubscription(si, callingPackage)) {
-                return;
-            }
+        // First check: does caller have carrier privilege?
+        if (tm != null && tm.hasCarrierPrivileges(subId)) {
+            return;
         }
 
         // Second check: has the CarrierService delegated access?

telephony/java/android/telephony/SubscriptionInfo.java
@@ -148,13 +148,14 @@ public class SubscriptionInfo implements Parcelable {
 
     /**
      * The access rules for this subscription, if it is embedded and defines any.
+     * This does not include access rules for non-embedded subscriptions.
      */
     @Nullable
     private UiccAccessRule[] mNativeAccessRules;
 
     /**
      * The carrier certificates for this subscription that are saved in carrier configs.
-     * The other carrier certificates are embedded on Uicc and stored as part of mNativeAccessRules.
+     * This does not include access rules from the Uicc, whether embedded or non-embedded.
      */
     @Nullable
     private UiccAccessRule[] mCarrierConfigAccessRules;
@@ -661,7 +662,6 @@ public class SubscriptionInfo implements Parcelable {
      * is authorized to manage this subscription.
      * TODO and fix it properly in R / master: either deprecate this and have 3 APIs
      *  native + carrier + all, or have this return all by default.
-     * @throws UnsupportedOperationException if this subscription is not embedded.
      * @hide
      */
     @SystemApi

telephony/java/android/telephony/SubscriptionManager.java
@@ -2631,6 +2631,10 @@ public class SubscriptionManager {
      * Checks whether the app with the given context is authorized to manage the given subscription
      * according to its metadata.
      *
+     * Only supported for embedded subscriptions (if {@link SubscriptionInfo#isEmbedded} returns
+     * true). To check for permissions for non-embedded subscription as well,
+     * {@see android.telephony.TelephonyManager#hasCarrierPrivileges}.
+     *
      * @param info The subscription to check.
      * @return whether the app is authorized to manage this subscription per its metadata.
      */
@@ -2642,6 +2646,10 @@ public class SubscriptionManager {
      * Checks whether the given app is authorized to manage the given subscription. An app can only
      * be authorized if it is included in the {@link android.telephony.UiccAccessRule} of the
      * {@link android.telephony.SubscriptionInfo} with the access status.
+     *
+     * Only supported for embedded subscriptions (if {@link SubscriptionInfo#isEmbedded} returns
+     * true). To check for permissions for non-embedded subscription as well,
+     * {@see android.telephony.TelephonyManager#hasCarrierPrivileges}.
      *
      * @param info The subscription to check.
      * @param packageName Package name of the app to check.

telephony/java/android/telephony/TelephonyManager.java
@@ -8534,6 +8534,9 @@ public class TelephonyManager {
      * call will return true. This access is granted by the owner of the UICC
      * card and does not depend on the registered carrier.
      *
+     * Note that this API applies to both physical and embedded subscriptions and
+     * is a superset of the checks done in SubscriptionManager#canManageSubscription.
+     *
      * @return true if the app has carrier privileges.
      */
     public boolean hasCarrierPrivileges() {
@@ -8546,6 +8549,9 @@ public class TelephonyManager {
      * If any of the packages in the calling UID has carrier privileges, the
      * call will return true. This access is granted by the owner of the UICC
      * card and does not depend on the registered carrier.
+     *
+     * Note that this API applies to both physical and embedded subscriptions and
+     * is a superset of the checks done in SubscriptionManager#canManageSubscription.
      *
      * @param subId The subscription to use.
      * @return true if the app has carrier privileges.

commit 691fb0a23554710397b1b9a6555b28f867a05e57

Carrier app with carrier privileges can't access SubscriptionPlan APIs
SubscriptionPlan APIs below are designed for carrier apps with carrier
privileges:
- SubscriptionManager.set/getSubscriptionPlan
- SubscriptionManager.setSubscriptionOverrideUnmetered
- SubscriptionManager.setSubscriptionOverrideCongested

All those APIs were broken in a history fix (with SHA e517138781d57432ef8360268dc91d41b70cb313 introduced into RVC-QPR release) which try to call
TelephonyManager.hasCarrierPrivileges inside NetworkPolicyManagerService
to check if the carrier app has carrier privileges. hasCarrierPrivileges
here actually check if NetworkPolicyManagerService has carrier
privileges and always returns false.

The fix is to check carrier privileges against the target calling
package with utility method
TelephonyManager.checkCarrierPrivilegesForPackage.

Basic CTS test cases are introduced to verify the behavior.

Bug: 377961753
Test: atest CarrierApiTest
Flag: EXEMPT (minor bug fix)

Change-Id: If78232dc9fc8f588c5b8b1c8fe7be44b09a4d207

services/core/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -3612,13 +3612,16 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         final long token = Binder.clearCallingIdentity();
         try {
             config = mCarrierConfigManager.getConfigForSubId(subId);
-            tm = mContext.getSystemService(TelephonyManager.class);
+            tm = mContext.getSystemService(TelephonyManager.class).createForSubscriptionId(subId);
         } finally {
             Binder.restoreCallingIdentity(token);
         }
 
-        // First check: does caller have carrier privilege?
-        if (tm != null && tm.hasCarrierPrivileges(subId)) {
+        // First check: does callingPackage have carrier privilege?
+        // Note that we can't call TelephonyManager.hasCarrierPrivileges() which will check if
+        // ourself has carrier privileges
+        if (tm != null && (tm.checkCarrierPrivilegesForPackage(callingPackage)
+                == TelephonyManager.CARRIER_PRIVILEGE_STATUS_HAS_ACCESS)) {
             return;
         }
 