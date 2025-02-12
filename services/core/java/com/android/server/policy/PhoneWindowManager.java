e9350cac749a3308f5f3bb26883b9b63091c3238
Wrdn28 <ifloraxd@gmail.com>
2025-02-12 05:01:57 +0000

    base: Fix duplicate declaration
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -869,8 +869,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mLongSwipeDown;
     private CameraAvailbilityListener mCameraAvailabilityListener;
 
-    private CameraAvailbilityListener mCameraAvailabilityListener;
-
     private class PolicyHandler extends Handler {
 
         private PolicyHandler(Looper looper) {
