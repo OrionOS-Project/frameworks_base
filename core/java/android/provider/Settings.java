35334e2d969c3ba5fcc6f9ee6343d8af08e1bc9f
NurKeinNeid <nurkeinneid@derpfest.org>
2025-02-22 16:14:01 +0100

    SystemUI: Add ongoing action chip toggle setting [1/2]
    Change-Id: I2083b5184d9a8233722661e42d7d5f33876fc276
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6958,6 +6958,11 @@ public final class Settings {
         public static final String UDFPS_ICON = "udfps_icon";
 
         /**
+         * @hide
+         */
+        public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
