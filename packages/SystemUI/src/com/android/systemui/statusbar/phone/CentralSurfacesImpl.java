f1bdfc840c906d87a0dc201a52dbf262d9be4fa3
f104a <f104a@f104a.io>
2025-02-27 16:17:47 +0100

    SystemUI: Hide ongoing action progress chip on lockscreen
    Change-Id: Iace5817e4c3111d23a1c38c0707e4822ce8fd93c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1271,7 +1271,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                              new OnGoingActionProgressController(
                                      mContext,
                                      statusBarViewController.getOngoingActionProgressGroup(),
-                                     mNotificationListener);
+                                     mNotificationListener, mKeyguardStateController);
                     });
         }
         if (!StatusBarRootModernization.isEnabled() && !StatusBarConnectedDisplays.isEnabled()) {
