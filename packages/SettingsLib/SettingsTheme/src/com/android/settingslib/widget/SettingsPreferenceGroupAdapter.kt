f17d5dfe3ce01afad208c5094f2d5a9194364b76
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-30 08:14:22 +0800

    SettingsLib: Exclude some classes
    Change-Id: I5f6029beff7d03f30b75f9a668f00181508ce8d5
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- packages/SettingsLib/SettingsTheme/src/com/android/settingslib/widget/SettingsPreferenceGroupAdapter.kt
+++ packages/SettingsLib/SettingsTheme/src/com/android/settingslib/widget/SettingsPreferenceGroupAdapter.kt
@@ -48,6 +48,10 @@ open class SettingsPreferenceGroupAdapter(preferenceGroup: PreferenceGroup) :
     private val mHandler = Handler(Looper.getMainLooper())
 
     private val syncRunnable = Runnable { updatePreferencesList() }
+    
+    private val excludedClasses = setOf(
+        
+    )
 
     init {
         val context = preferenceGroup.context
@@ -111,7 +115,17 @@ open class SettingsPreferenceGroupAdapter(preferenceGroup: PreferenceGroup) :
         var endIndex = -1
         var currentParent: PreferenceGroup? = group
         for (i in 0 until itemCount) {
-            when (val pref = getItem(i)) {
+            val pref = getItem(i)
+            val isExcludedFromExpressive = pref?.javaClass?.name in excludedClasses
+            if (isExcludedFromExpressive) {
+                cornerStyles[i] = 0
+                startIndex = -1
+                endIndex = -1
+                currentParent = pref?.parent as? PreferenceGroup ?: currentParent
+                continue
+            }
+
+            when (pref) {
                 // the preference has round corner background, so we don't need to handle it.
                 is GroupSectionDividerMixin -> {
                     cornerStyles[i] = 0
@@ -176,11 +190,22 @@ open class SettingsPreferenceGroupAdapter(preferenceGroup: PreferenceGroup) :
     /** handle roundCorner background */
     private fun updateBackground(holder: PreferenceViewHolder, position: Int) {
         val context = holder.itemView.context
+        val pref = getItem(position)
+
+        val isExcludedFromExpressive = pref?.javaClass?.name in excludedClasses
+
         @DrawableRes
         val backgroundRes =
-            when (SettingsThemeHelper.isExpressiveTheme(context)) {
-                true -> getRoundCornerDrawableRes(position, isSelected = false)
-                else -> mLegacyBackgroundRes
+            when {
+                SettingsThemeHelper.isExpressiveTheme(context) && isExcludedFromExpressive -> {
+                    mLegacyBackgroundRes
+                }
+                SettingsThemeHelper.isExpressiveTheme(context) -> {
+                    getRoundCornerDrawableRes(position, isSelected = false)
+                }
+                else -> {
+                    mLegacyBackgroundRes
+                }
             }
 
         val v = holder.itemView
@@ -189,7 +214,7 @@ open class SettingsPreferenceGroupAdapter(preferenceGroup: PreferenceGroup) :
         v.setBackgroundResource(backgroundRes)
 
         // Update padding
-        if (SettingsThemeHelper.isExpressiveTheme(context)) {
+        if (SettingsThemeHelper.isExpressiveTheme(context) && !isExcludedFromExpressive) {
             val (paddingStart, paddingEnd) = getStartEndPadding(position, backgroundRes)
             v.setPaddingRelative(paddingStart, v.paddingTop, paddingEnd, v.paddingBottom)
             v.clipToOutline = backgroundRes != 0
