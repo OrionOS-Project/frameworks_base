3b33bf6e130c962e170efa1f514129c8b31b6522
NurKeinNeid <nurkeinneid@derpfest.org>
2025-07-13 02:09:39 +0200

    Update UdfpsAnimation constructor to accept KeyguardStateController

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -801,7 +801,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
 
         if (isAnimationPackageInstalled()) {
-            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController, mKeyguardStateController);
         }
 
         updateUdfpsAnimation();
@@ -1217,7 +1217,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 mUdfpsAnimation.removeAnimation();
                 mUdfpsAnimation = null;
             }
-            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController);
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps, mAuthController, mKeyguardStateController);
             if (mUdfpsAnimation != null) {
                 mUdfpsAnimation.updatePosition();
             }
