OrionOS-Project/frameworks_base

commit e2286dfd88aba2a17a24a8d89e01e78ed647d84a

Persist high-resolution snapshots using lossless compression
The lossless compression approach provides more reliable results
against the HumanEyeMatcher comparison method.

Flag: com.android.window.flags.respect_requested_task_snapshot_resolution
Bug: 438072503
Test: nexus_image_test_platform
Change-Id: I9b736d2a444587b20fe8b82464805f568106981c

services/core/java/com/android/server/wm/SnapshotPersistQueue.java
@@ -17,6 +17,7 @@
 package com.android.server.wm;
 
 import static android.graphics.Bitmap.CompressFormat.JPEG;
+import static android.graphics.Bitmap.CompressFormat.PNG;
 import static android.os.Trace.TRACE_TAG_WINDOW_MANAGER;
 
 import static com.android.server.wm.WindowManagerDebugConfig.TAG_WITH_CLASS_NAME;
@@ -509,7 +510,8 @@ class SnapshotPersistQueue {
 
             final File lowResFile = mPersistInfoProvider.getLowResolutionBitmapFile(mId, mUserId);
             try (FileOutputStream lowResFos = new FileOutputStream(lowResFile)) {
-                lowResBitmap.compress(JPEG, COMPRESS_QUALITY, lowResFos);
+                lowResBitmap.compress(Flags.respectRequestedTaskSnapshotResolution() ? PNG : JPEG,
+                        COMPRESS_QUALITY, lowResFos);
             } catch (IOException e) {
                 Slog.e(TAG, "Unable to open " + lowResFile + " for persisting.", e);
                 return false;

commit 5a56733451477068623401315cbb4553cfcf701e

Correct lossless compression target
The previous change e2286df mistakenly applied lossless compression to
low-resolution snapshots. This fix ensures that compression is
correctly applied to high-resolution snapshots as intended.

Flag: com.android.window.flags.respect_requested_task_snapshot_resolution
Bug: 438072503
Test: nexus_image_test_platform
Change-Id: Ie10e519a7a2ec844ba5b82bea139a5d53b3c89ea

services/core/java/com/android/server/wm/SnapshotPersistQueue.java
@@ -481,7 +481,8 @@ class SnapshotPersistQueue {
             }
             final File file = mPersistInfoProvider.getHighResolutionBitmapFile(mId, mUserId);
             try (FileOutputStream fos = new FileOutputStream(file)) {
-                swBitmap.compress(JPEG, COMPRESS_QUALITY, fos);
+                swBitmap.compress(Flags.respectRequestedTaskSnapshotResolution() ? PNG : JPEG,
+                        COMPRESS_QUALITY, fos);
             } catch (IOException e) {
                 Slog.e(TAG, "Unable to open " + file + " for persisting.", e);
                 return false;
@@ -510,8 +511,7 @@ class SnapshotPersistQueue {
 
             final File lowResFile = mPersistInfoProvider.getLowResolutionBitmapFile(mId, mUserId);
             try (FileOutputStream lowResFos = new FileOutputStream(lowResFile)) {
-                lowResBitmap.compress(Flags.respectRequestedTaskSnapshotResolution() ? PNG : JPEG,
-                        COMPRESS_QUALITY, lowResFos);
+                lowResBitmap.compress(JPEG, COMPRESS_QUALITY, lowResFos);
             } catch (IOException e) {
                 Slog.e(TAG, "Unable to open " + lowResFile + " for persisting.", e);
                 return false;