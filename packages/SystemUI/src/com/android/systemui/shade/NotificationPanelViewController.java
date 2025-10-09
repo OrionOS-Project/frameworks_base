e912e0f9cdbf1b68f4fef9a4f0ae66a569fbe09c
DaViDev985 <davidev985@gmail.com>
2025-10-09 18:39:48 +0200

    Revert "SystemUI: Show island notification on landscape"
    This reverts commit 31f1fb801042e6b89f79d8ef6ddd69f7c517c0db.
    On previous Android versions, landscape HUNs were broken if using split-shade QS, so we used island notifications in that scenario as a workaround.
    With Android 16, that issue seemingly got fixed, so there's no need for this anymore.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3847,9 +3847,11 @@ public final class NotificationPanelViewController implements
             switch (key) {
                 case ISLAND_NOTIFICATION:
                     mUseIslandNotification = TunerService.parseIntegerSwitch(newValue, false);
+                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
                     break;
                 case HEADS_UP_NOTIFICATIONS_ENABLED:
                     mUseHeadsUp = TunerService.parseIntegerSwitch(newValue, false);
+                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
                     break;
                 case STATUS_BAR_CUSTOM_HEADER:
                     mHeaderImageEnabled =
@@ -4525,20 +4527,15 @@ public final class NotificationPanelViewController implements
     public void showIsland(boolean show) {
         // if landNotify is showing, it must disappear for a while      -- alphi-wang-cn
         if (/* must dismiss if not show! */ !show
-                || useIslandNotification() && mUseHeadsUp) {
+                || mUseIslandNotification && mUseHeadsUp) {
             mNotifIsland.showIsland(show, getExpandedFraction());
         }
     }
 
     protected void updateIslandVisibility() {
-        if (useIslandNotification() && mUseHeadsUp) {
+        if (mUseIslandNotification && mUseHeadsUp) {
             mNotifIsland.updateIslandVisibility(getExpandedFraction());
         }
-    }
-
-    private boolean useIslandNotification() {
-        return mUseIslandNotification || mView.getContext().getResources().getConfiguration().orientation 
-            == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     private void doUpdateStatusBarCustomHeader(Drawable drawable, boolean force) {
