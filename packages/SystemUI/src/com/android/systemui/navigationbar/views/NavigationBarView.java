060f82077376a94158e0aa3ff7fb18e4c8ba1490
NurKeinNeid <nurkeinneid@derpfest.org>
2025-10-22 01:32:12 +0200

    SystemUI: NavigationBarView: Properly respect user settings for navbar hint visibility
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/navigationbar/views/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/views/NavigationBarView.java
@@ -92,6 +92,8 @@ import com.android.systemui.utils.windowmanager.WindowManagerUtils;
 import com.android.wm.shell.back.BackAnimation;
 import com.android.wm.shell.pip.Pip;
 
+import lineageos.providers.LineageSettings;
+
 import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Optional;
@@ -673,7 +675,13 @@ public class NavigationBarView extends FrameLayout {
         getBackButton().setVisibility(disableBack       ? View.INVISIBLE : View.VISIBLE);
         getHomeButton().setVisibility(disableHome       ? View.INVISIBLE : View.VISIBLE);
         getRecentsButton().setVisibility(disableRecent  ? View.INVISIBLE : View.VISIBLE);
-        getHomeHandle().setVisibility(disableHomeHandle || mHomeHandleForceHidden ? View.INVISIBLE : View.VISIBLE);
+        
+        // Check if user has disabled navbar hint in settings
+        boolean navbarHintEnabled = LineageSettings.System.getInt(
+                mContext.getContentResolver(),
+                LineageSettings.System.NAVIGATION_BAR_HINT, 1) == 1;
+        
+        getHomeHandle().setVisibility(disableHomeHandle || mHomeHandleForceHidden || !navbarHintEnabled ? View.INVISIBLE : View.VISIBLE);
         notifyActiveTouchRegions();
     }
 
@@ -682,7 +690,13 @@ public class NavigationBarView extends FrameLayout {
         boolean disableRecent = isRecentsButtonDisabled() | !QuickStepContract.isLegacyMode(mNavBarMode);
         boolean disableHomeHandle = disableRecent
                 && ((mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0);
-        getHomeHandle().setVisibility(disableHomeHandle || hide ? View.INVISIBLE : View.VISIBLE);
+        
+        // Check if user has disabled navbar hint in settings
+        boolean navbarHintEnabled = LineageSettings.System.getInt(
+                mContext.getContentResolver(),
+                LineageSettings.System.NAVIGATION_BAR_HINT, 1) == 1;
+        
+        getHomeHandle().setVisibility(disableHomeHandle || hide || !navbarHintEnabled ? View.INVISIBLE : View.VISIBLE);
     }
 
     public boolean isHomeHandleForceHidden() {
