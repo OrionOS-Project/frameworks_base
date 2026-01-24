774c42dda2503436f161a4275104e234e331564a
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-24 11:53:52 +0100

    fixup! SystemUI: add FloatingRotationButton for hw-key devices

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -41,6 +41,7 @@ import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.PluginManager;
 import com.android.systemui.plugins.VolumeDialogController;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.rotation.RotationPolicyWrapper;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.NotificationListener;
@@ -157,6 +158,7 @@ public class Dependency {
     @Inject Lazy<UserTracker> mUserTrackerLazy;
     @Inject Lazy<StatusBarWindowControllerStore> mStatusBarWindowControllerStoreLazy;
     @Inject Lazy<SysUIStateDisplaysInteractor> mSysUIStateDisplaysInteractor;
+    @Inject Lazy<RotationPolicyWrapper> mRotationPolicyWrapperLazy;
 
     @Inject
     public Dependency() {
@@ -206,6 +208,7 @@ public class Dependency {
         mProviders.put(SysUIStateDisplaysInteractor.class, mSysUIStateDisplaysInteractor::get);
         mProviders.put(
                 StatusBarWindowControllerStore.class, mStatusBarWindowControllerStoreLazy::get);
+        mProviders.put(RotationPolicyWrapper.class, mRotationPolicyWrapperLazy::get);
 
         Dependency.setInstance(this);
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -24,7 +24,6 @@ import android.graphics.Insets;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.inputmethodservice.InputMethodService;
-import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -62,6 +61,8 @@ import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.user.ui.binder.StatusBarUserChipViewBinder;
 import com.android.systemui.user.ui.viewmodel.StatusBarUserChipViewModel;
 import com.android.systemui.util.leak.RotationUtils;
+import com.android.systemui.Dependency;
+import com.android.systemui.rotation.RotationPolicyWrapper;
 
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
@@ -127,8 +128,11 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
                     R.dimen.floating_rotation_button_diameter, R.dimen.key_button_ripple_max_width,
                     R.bool.floating_rotation_button_position_left);
 
-            mRotationButtonController = new RotationButtonController(lightContext, lightIconColor,
-                    darkIconColor, R.drawable.ic_sysbar_rotate_button_ccw_start_0,
+            final RotationPolicyWrapper rotationPolicyWrapper =
+                    Dependency.get(RotationPolicyWrapper.class);
+            mRotationButtonController = new RotationButtonController(rotationPolicyWrapper,
+                    lightContext, lightIconColor, darkIconColor,
+                    R.drawable.ic_sysbar_rotate_button_ccw_start_0,
                     R.drawable.ic_sysbar_rotate_button_ccw_start_90,
                     R.drawable.ic_sysbar_rotate_button_cw_start_0,
                     R.drawable.ic_sysbar_rotate_button_cw_start_90,
